929670afd0e3d890e70fb4e9b0fd63a8b3190fe0
Gopal <gopalpokhrel101@gmail.com>
2023-06-04 14:01:03 +0545

    JS

---
+++ script.js
@@ -0,0 +1,7 @@
+let moon = document.querySelector("#one");
+
+moon.addEventListener('click', ()=>{
+    let container = document.querySelector('.container');
+
+    container.classList.toggle("dark");
+})
